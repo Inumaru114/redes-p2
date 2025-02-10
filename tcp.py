e99355aa7e709128d954811d5c00fb5d4cc4baae
Lucas Velloso <lucas.velloso@dilettasolutions.com>
2025-02-10 16:07:57 -0300

    Passo 4

--- tcp.py
+++ tcp.py
@@ -56,7 +56,8 @@ class Servidor:
             print('%s:%d -> %s:%d (pacote associado a conexão desconhecida)' %
                   (src_addr, src_port, dst_addr, dst_port))
 
-
+def redefineAddress(id_conexao):
+    return id_conexao[0],id_conexao[1],id_conexao[2],id_conexao[3],
 class Conexao:
     def __init__(self, servidor, id_conexao, seq_no):
         self.servidor = servidor
@@ -70,6 +71,7 @@ class Conexao:
         # Esta função é só um exemplo e pode ser removida
         print('Este é um exemplo de como fazer um timer')
 
+
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
         if (seq_no != self.ack_no) and len(payload) != 0:
 
@@ -78,11 +80,8 @@ class Conexao:
             self.ack_no = seq_no + len(payload)
 
             if (len(payload) != 0) or (flags & FLAGS_SYN) or (flags & FLAGS_FIN):
-                src_port = self.id_conexao[1]
-                dst_port = self.id_conexao[3]
+                dst_address, src_port, src_address, dst_port = redefineAddress(self.id_conexao)
                 header = make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_ACK)
-                src_address = self.id_conexao[2]
-                dst_address = self.id_conexao[0]
                 self.servidor.rede.enviar(fix_checksum(header, src_address, dst_address), src_address)
 
             self.callback(self, payload)
@@ -103,8 +102,6 @@ class Conexao:
         """
         Usado pela camada de aplicação para enviar dados
         """
-        # TODO: implemente aqui o envio de dados.
-
         src_port, dst_port, seq_no, ack_no, flags, window_size, checksum, urg_ptr = read_header(dados)
 
         if (flags& FLAGS_ACK == FLAGS_ACK):
@@ -112,12 +109,8 @@ class Conexao:
         
         data_length = len(dados)
         bytes_sent = 0
-
-        src_port = self.id_conexao[1]
-        dst_port = self.id_conexao[3]
-
-        src_address = self.id_conexao[2]
-        dst_address = self.id_conexao[0]
+        
+        dst_address, src_port, src_address, dst_port = redefineAddress(self.id_conexao)
 
         if (bytes_sent < data_length) :
             while (bytes_sent < data_length) :
@@ -136,5 +129,11 @@ class Conexao:
         """
         Usado pela camada de aplicação para fechar a conexão
         """
-        # TODO: implemente aqui o fechamento de conexão
+    
+        dst_address, src_port, src_address, dst_port = redefineAddress(self.id_conexao)
+        header = make_header(dst_port, src_port, self.seq_no + 1, self.ack_no, FLAGS_FIN)
+        self.servidor.rede.enviar(fix_checksum(header, src_address, dst_address), src_address)
+
         pass
+
+
