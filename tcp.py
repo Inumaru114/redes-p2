1addfab50698d8d6774ad9fed54373ebb2823378
Lucas Velloso <lucas.velloso@dilettasolutions.com>
2025-02-10 14:19:06 -0300

    Passos 1 e 2

--- tcp.py
+++ tcp.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from tcputils import *
 
 
@@ -8,6 +9,8 @@ class Servidor:
         self.porta = porta
         self.conexoes = {}
         self.callback = None
+        self.seq_no = os.urandom()
+        self.ack_no = self.seq_no + 1
         self.rede.registrar_recebedor(self._rdt_rcv)
 
     def registrar_monitor_de_conexoes_aceitas(self, callback):
@@ -34,9 +37,16 @@ class Servidor:
         if (flags & FLAGS_SYN) == FLAGS_SYN:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
-            # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
-            # fazer aqui mesmo ou dentro da classe Conexao.
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no)
+
+            # Constroi o ack
+            seq_no = conexao.seq_no
+            ack_no = seq_no + 1
+            header = make_header(self.porta, src_port, seq_no, ack_no, FLAGS_SYN | FLAGS_ACK)
+
+            #Envia o ack;:
+            self.rede.enviar(fix_checksum(header, src_addr, dst_addr), src_addr)
+            
             if self.callback:
                 self.callback(conexao)
         elif id_conexao in self.conexoes:
@@ -48,9 +58,10 @@ class Servidor:
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao, seq_no):
         self.servidor = servidor
         self.id_conexao = id_conexao
+        self.seq_no = seq_no
         self.callback = None
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
@@ -60,9 +71,22 @@ class Conexao:
         print('Este é um exemplo de como fazer um timer')
 
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
-        # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
-        # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
-        # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
+        if (seq_no != self.ack_no):
+
+            return
+        else :
+            self.ack_no = seq_no + len(payload)
+
+            src_port = self.id_conexao[1]
+            dst_port = self.id_conexao[3]
+            header = make_header(dst_port, src_port, self.seq_no, self.ack_no, FLAGS_ACK)
+            src_address = self.id_conexao[2]
+            dst_address = self.id_conexao[0]
+            self.servidor.rede.enviar(fix_checksum(header, src_address, dst_address), src_address)
+
+            self.callback(self, payload)
+
+
         print('recebido payload: %r' % payload)
 
     # Os métodos abaixo fazem parte da API
